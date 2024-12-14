0b422f1ea5f8f0d719121f9abdb33c1751309d49
Anmol Jindal <anmoljindal025@gmail.com>
2024-12-14 14:28:55 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A MLOps Project to do sentiment analysis on Youtube Comments',
+    author='Anmol Jindal',
+    license='MIT',
+)
